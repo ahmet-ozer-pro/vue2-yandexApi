1d03bdbcfb594fe85cb3b27a5b77e4f80846cab7
Ahmet Özer <aozer@turksat.local>
2021-06-03 21:11:22 +0300

    Component Yapısının Olusturulması

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <template>
   <div class="container">
     <h1 class="text-center">Vue-Translator</h1>
-    <h5 class="text-center">Vue.js ile Translator</h5>
-    <br>
-    <div class="row">
-      <div class="col-md-12 text-center">
-        <TranslateForm
-          @historyEvent="history.push($event)"
-          @translatedEvent="translatedText = $event"/>
-        <h3 class="text-success text-center"> {{ translatedText }}</h3>
-        <SearchHistory :history="history"  />
-      </div>
-    </div>
+    <h5 class="text-center">Vue.js ile Tran</h5>
   </div>
 </template>
-<script>
-  import TranslateForm from "./components/TranslateForm"
-  import SearchHistory from "./components/SearchHistory"
 
-  export default {
-    components : {
-      TranslateForm,
-      SearchHistory
-    },
-    data(){
-      return {
-        translatedText : '',
-        history : []
-      }
-    },
-  }
-</script>
-<style>
-  body{
-    background-color: #dedede;
-  }
-</style>
+<script></script>
+<style></style>
