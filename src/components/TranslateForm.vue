6911c348c7ff83fe8f0c55d630ab7bd05e2a6a70
Ahmet Özer <aozer@turksat.local>
2021-06-04 10:24:13 +0300

    Desteklenen Dillerin Cekilmesi

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -8,7 +8,9 @@
       placeholder="Çevirmek istediğiniz kelime/cümle yazınız."
     ></textarea>
     <select v-model="translateTo" class="form-control">
-      <option>1</option>
+      <option v-for="(value, key) in languages" :value="key">
+        {{ value }}
+      </option>
     </select>
     <br />
     <div class="text-left">
@@ -28,6 +30,7 @@ export default {
     return {
       translateText: '',
       translateTo: '',
+      languages: {},
     }
   },
   /* API key:
@@ -47,6 +50,18 @@ export default {
         .catch((e) => console.log(e))
       //url = https://translate.yandex.net/api/v1.5/tr.json/translate
     },
+  },
+  created() {
+    //desteklenen dillerin çekilmesi işlemi
+    //ui=desteklenen dillerin hangi dilde gösterilmesini sağlar
+    axios
+      .get(
+        'https://translate.yandex.net/api/v1.5/tr.json/getLang?key=trnsl.1.1.20180919T140412Z.20a6cc3cb2b92135.859b224f09389c0502296ae695358c150a77804c&ui=tr'
+      )
+      .then((response) => {
+        this.languages = response.data.langs
+      })
+      .catch((e) => console.log(e))
   },
 }
 </script>
