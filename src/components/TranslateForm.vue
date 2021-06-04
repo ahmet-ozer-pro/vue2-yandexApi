b3ed48f2700de6cadb0d41b070ad87d3c8daaaf3
Ahmet Özer <aozer@turksat.local>
2021-06-04 09:05:50 +0300

    Api key olusturma

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -29,10 +30,22 @@ export default {
       translateTo: '',
     }
   },
+  /* API key:
+  tmsl.1.120180919T140412Z.20a6cc3cb2b92135.859b224f09389c0502296ae695358c150a77804c
+  */
   methods: {
     translateIt() {
-      alert(this.translateText)
-      alert(this.translateTo)
+      let url =
+        'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180919T140412Z.20a6cc3cb2b92135.859b224f09389c0502296ae695358c150a77804c&text=' +
+        this.translateText +
+        '&lang=en'
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((e) => console.log(e))
+      //url = https://translate.yandex.net/api/v1.5/tr.json/translate
     },
   },
 }
